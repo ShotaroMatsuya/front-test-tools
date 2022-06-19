4b2e22b514b1abdb52e6a13f53cf83485e924554
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-06-19 16:27:28 +0900

    modify servers settings

--- app/src/pages/entry/ScoopOption.jsx
+++ app/src/pages/entry/ScoopOption.jsx
@@ -8,7 +8,7 @@ export default function ScoopOption({ name, imagePath, updateItemCount }) {
   };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-      <img src={`http://localhost:3030/${imagePath}`} alt={`${name} scoop`} />
+      <img src={`http://localhost:3030${imagePath}`} alt={`${name} scoop`} />
       <Form.Group
         controlId={`${name}-count`}
         as={Row}
