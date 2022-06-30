69665cc5fe9b3be77123b0c11f6b72cabe889e0a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-01 00:25:28 +0900

    test GET request to api

--- cypress-api-request/cypress/e2e/request.cy.js
+++ cypress-api-request/cypress/e2e/request.cy.js
@@ -1,5 +1,17 @@
 /// <reference types="Cypress" />
 
 describe('Request command suite', () => {
-  it('Get request', () => {});
+  it('Get request', () => {
+    cy.request('GET', 'http://localhost:8080/todos').then(response => {
+      cy.log(response.status);
+      cy.log(response.statusText);
+      cy.log(response.duration);
+      cy.log(response.headers);
+      cy.log(response.body);
+
+      expect(response.status).to.be.eq(200);
+      expect(response.duration).to.be.below(20000);
+      expect(response.body[0].isComplete).to.be.false;
+    });
+  });
 });
