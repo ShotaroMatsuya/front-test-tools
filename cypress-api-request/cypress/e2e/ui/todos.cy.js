cd3516b10f718923819e74b69a4438c9bd91b79d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-01 05:54:20 +0900

    test if todo is updated when checkbox is activated

--- cypress-api-request/cypress/e2e/ui/todos.cy.js
+++ cypress-api-request/cypress/e2e/ui/todos.cy.js
@@ -11,6 +11,12 @@ describe('Todo UI testing ', () => {
     cy.get('.todo-item').last().should('contain.text', 'First Todo');
   });
 
+  it('should be able to toggle the status of a todo correctly', () => {
+    cy.get('.todo-input').type('First Todo{enter}');
+    cy.get('.success').should('be.visible');
+    cy.get('.todo-checkbox').check().should('be.checked');
+    cy.get('.todo-checkbox').uncheck().should('not.be.checked');
+  });
   afterEach(() => {
     cy.get('.delete-item').click({ multiple: true });
   });
