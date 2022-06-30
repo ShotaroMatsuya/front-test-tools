063f5de30794462a54edfc89cd2a4da93e78ba8e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-01 06:43:43 +0900

    add filter functionality using api

--- cypress-api-request/cypress/e2e/ui/filter.cy.js
+++ cypress-api-request/cypress/e2e/ui/filter.cy.js
@@ -1,9 +1,22 @@
-describe('filter functionality test cases', () => {
+describe.only('filter functionality test cases', () => {
   before(() => {
     cy.addDummyTodos();
     cy.visit('/');
   });
-  it('should filter out the complete todos correctly', () => {});
+  it('should filter out the complete todos correctly', () => {
+    cy.contains('Complete').click();
+    cy.url().should('contain', '/complete');
+    cy.get('.todo-checkbox').each(element => {
+      cy.wrap(element).should('be.checked');
+    });
+  });
+  it('should filter out the active todos correctly', () => {
+    cy.contains('Active').click();
+    cy.url().should('contain', '/active');
+    cy.get('.todo-checkbox').each(element => {
+      cy.wrap(element).should('not.be.checked');
+    });
+  });
   after(() => {
     cy.get('body').then($el => {
       if ($el.find('.delete-item').length > 0) {
