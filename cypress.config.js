bf2dd69d95a2022dfb93c388fddf1faabe5a3dcc
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-03 17:58:49 +0900

    modify cypress config for root dir path adjustment

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,20 @@
+// module.exports = {
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// };
+
 module.exports = {
+  projectId: 'yn1ycf',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
   },
+  retries: 2,
+  fileServerFolder: 'cypress-api-request',
+  fixturesFolder: 'cypress-api-request/cypress/fixtures',
+  screenshotsFolder: 'cypress-api-request/cypress/screenshots',
+  videosFolder: 'cypress-api-request/cypress/videos',
 };
