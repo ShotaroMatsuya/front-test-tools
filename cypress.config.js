78ded35e472e852998568247a28d9a42c94cab17
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-03 18:37:00 +0900

    update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,8 +11,11 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
+    supportFile: 'cypress-api-request/cypress/support/e2e.{js,jsx,ts,tsx}',
+    specPattern: 'cypress-api-request/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
   retries: 2,
+  downloadsFolder: 'cypress-api-request/cypress/downloads',
   fileServerFolder: 'cypress-api-request',
   fixturesFolder: 'cypress-api-request/cypress/fixtures',
   screenshotsFolder: 'cypress-api-request/cypress/screenshots',
