dde94e9d0e61fec9a1070a176c531e3ad7abd1c0
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-16 02:29:51 +0900

    add cypress.config

---
+++ cypress-api-request/cypress.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projectId: 'yn1ycf',
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+    supportFile: '../e2e/cypress/support/e2e.{js,jsx,ts,tsx}',
+    specPattern: '../e2e/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  retries: 2,
+  downloadsFolder: '../e2e/cypress/downloads',
+  fixturesFolder: '../e2e/cypress/fixtures',
+  screenshotsFolder: '../e2e/cypress/screenshots',
+  videosFolder: '../e2e/cypress/videos',
+};
