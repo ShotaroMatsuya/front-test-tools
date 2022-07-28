6cda8bc58e0bff19c436092d5d1036090781dbe8
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-28 23:00:20 +0900

    Test case for normal users

--- cypress-project/cypress/e2e/courses.cy.js
+++ cypress-project/cypress/e2e/courses.cy.js
@@ -11,14 +11,24 @@ describe('Check courses functionality', () => {
       CourseComponent.deleteButton().should('be.visible');
     });
 
-    it('should be able to add a new task correctly', () => {
+    it('should be able to add a new course correctly', () => {
       CourseComponent.newCourseInput().type('Learn Selenium');
       CourseComponent.newCourseButton().click();
       CourseComponent.deleteButton().should('have.length', 3);
     });
-    it('should be able to delete a task', () => {
+    it('should be able to delete a course', () => {
       CourseComponent.deleteButton().last().click();
       CourseComponent.deleteButton().should('have.length', 2);
+    });
+  });
+
+  context('Check the functionality for an admin user', () => {
+    beforeEach(() => {
+      Utils.loginWithAPI('user', 'user');
+      Utils.visitAPage('courses');
+    });
+    it('normal user should be able to see all the courses', () => {
+      CourseComponent.courseName().should('have.length', 2);
     });
   });
 });
