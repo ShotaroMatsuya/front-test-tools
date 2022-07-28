6e866dd0cfb6e31ce4f1005e902f4a5055dcf895
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-28 20:40:44 +0900

    add Utils file in support

--- cypress-project/cypress/components/header_component.js
+++ cypress-project/cypress/components/header_component.js
@@ -1,1 +1,2 @@
 export const logoutButton = () => cy.get('[data-cy=signout-button]');
+export const coursesButton = () => cy.get('[href="/courses"]');
--- cypress-project/cypress/e2e/login.cy.js
+++ cypress-project/cypress/e2e/login.cy.js
@@ -1,22 +1,23 @@
 import * as LoginComponent from '../components/login_component';
 import * as HeaderComponent from '../components/header_component';
 import * as CourseComponent from '../components/course_component';
+import * as Utils from '../support/utils';
 
 describe('Test all login functionalities', () => {
   beforeEach(() => {
-    cy.visit('/login');
+    Utils.visitAPage('login');
   });
 
   it('login with valid email and password for a normal user', () => {
     LoginComponent.performLogin('user', 'user');
-    cy.get('[href="/courses"]').click();
-    cy.url().should('contain', '/courses');
+    HeaderComponent.coursesButton().click();
+    Utils.checkURL().should('contain', '/courses');
     HeaderComponent.logoutButton().should('be.visible');
   });
   it('should login correctly as admin user', () => {
     LoginComponent.performLogin('admin', 'admin');
-    cy.get('[href="/courses"]').click();
-    cy.url().should('contain', '/courses');
+    HeaderComponent.coursesButton().click();
+    Utils.checkURL().should('contain', '/courses');
 
     HeaderComponent.logoutButton().should('be.visible');
     CourseComponent.deleteButton().should('be.visible');
