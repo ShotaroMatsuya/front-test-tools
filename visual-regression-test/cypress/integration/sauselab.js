dd8e5655d192848f75f11b11ccd4e3b7cd336a40
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-29 12:27:25 +0900

    update the gbase snapshots

--- visual-regression-test/cypress/integration/sauselab.js
+++ visual-regression-test/cypress/integration/sauselab.js
@@ -2,13 +2,14 @@
 
 describe('SauseLab Landing page', () => {
   beforeEach(() => {
-    // cy.viewport('macbook-15');
+    cy.viewport('macbook-15');
     cy.visit('https://www.saucedemo.com');
   });
   it('should load the page correctly', () => {
     cy.matchImageSnapshot('Full Page'); // 最初はsnapshotを作成し、以降のテストではsnapshotと比較する
   });
   it('the logo should be displayed correctly', () => {
-    cy.get('.login_logo').matchImageSnapshot('logo');
+    cy.get('.login_logo').matchImageSnapshot('logo'); //特定の要素をsnapshotする
+    // base画像をupdateしたいときは、npx cypress run --env updateSnapshots=trueを実行する
   });
 });
