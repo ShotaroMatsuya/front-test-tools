81819302bd75a5f0b4a5b22242dcb6cd3b281a7e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-07-29 12:32:34 +0900

    visual regression test on different screen sizes

--- visual-regression-test/cypress/integration/sauselab.js
+++ visual-regression-test/cypress/integration/sauselab.js
@@ -1,15 +1,17 @@
 /// <reference types="Cypress" />
 
-describe('SauseLab Landing page', () => {
-  beforeEach(() => {
-    cy.viewport('macbook-15');
-    cy.visit('https://www.saucedemo.com');
-  });
-  it('should load the page correctly', () => {
-    cy.matchImageSnapshot('Full Page'); // 最初はsnapshotを作成し、以降のテストではsnapshotと比較する
-  });
-  it('the logo should be displayed correctly', () => {
-    cy.get('.login_logo').matchImageSnapshot('logo'); //特定の要素をsnapshotする
-    // base画像をupdateしたいときは、npx cypress run --env updateSnapshots=trueを実行する
+['macbook-15', 'ipad-2', 'iphone-x'].forEach(screen => {
+  describe('SauseLab Landing page', () => {
+    beforeEach(() => {
+      cy.viewport(screen);
+      cy.visit('https://www.saucedemo.com');
+    });
+    it('should load the page correctly', () => {
+      cy.matchImageSnapshot(`Full Page on ${screen}`); // 最初はsnapshotを作成し、以降のテストではsnapshotと比較する
+    });
+    it('the logo should be displayed correctly', () => {
+      cy.get('.login_logo').matchImageSnapshot(`logo on ${screen}`); //特定の要素をsnapshotする
+      // base画像をupdateしたいときは、npx cypress run --env updateSnapshots=trueを実行する
+    });
   });
 });
